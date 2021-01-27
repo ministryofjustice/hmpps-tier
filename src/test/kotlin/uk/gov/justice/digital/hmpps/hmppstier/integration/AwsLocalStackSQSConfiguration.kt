f28c47c56473216abd7260a1f78f52d7c6c8fd6f
Mark Rees <mark.rees@digital.justice.gov.uk>
2021-01-27 10:33:30 +0000

    MAS-274 don't use localstack for running tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppstier/integration/AwsLocalStackSQSConfiguration.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppstier/integration/AwsLocalStackSQSConfiguration.kt
@@ -1,23 +1,14 @@
 package uk.gov.justice.digital.hmpps.hmppstier.integration
 
-import com.amazonaws.services.sqs.AmazonSQSAsync
-import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
-import org.springframework.context.annotation.Bean
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.cloud.aws.messaging.listener.SimpleMessageListenerContainer
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
-import org.testcontainers.containers.localstack.LocalStackContainer
 
 @Configuration
-@ConditionalOnProperty(name = ["offender-events.sqs-provider"], havingValue = "localstack-embedded")
-class AwsLocalStackSQSConfiguration(private val localStackContainer: LocalStackContainer) {
+@ConditionalOnProperty(name = ["offender-events.sqs-provider"], havingValue = "mocked")
+class AwsLocalStackSQSConfiguration() {
 
-  @Primary
-  @Bean
-  fun localstackTestClient(localStackContainer: LocalStackContainer): AmazonSQSAsync {
-    return AmazonSQSAsyncClientBuilder.standard()
-      .withEndpointConfiguration(localStackContainer.getEndpointConfiguration(LocalStackContainer.Service.SQS))
-      .withCredentials(localStackContainer.defaultCredentialsProvider)
-      .build()
-  }
+  @MockBean
+  var simpleMessageListenerContainer: SimpleMessageListenerContainer? = null
 }
