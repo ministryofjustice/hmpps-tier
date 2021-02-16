f0ff9ca4fc75dd6026a7b5af79c09813c9f04bea
Nick McMahon <nicholas.mcmahon@digital.justice.gov.uk>
2021-02-16 10:23:34 +0000

    Postgres JSONB fix attempt.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/domain/TierLevel.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/domain/TierLevel.kt
@@ -1,6 +1,8 @@
 package uk.gov.justice.digital.hmpps.hmppstier.domain
 
+import java.io.Serializable
+
 data class TierLevel<E : Enum<E>>(
   val tier: E,
   val points: Int,
-)
+) : Serializable
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/jpa/entity/TierCalculationResultEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/jpa/entity/TierCalculationResultEntity.kt
@@ -3,8 +3,9 @@ package uk.gov.justice.digital.hmpps.hmppstier.jpa.entity
 import uk.gov.justice.digital.hmpps.hmppstier.domain.TierLevel
 import uk.gov.justice.digital.hmpps.hmppstier.domain.enums.ChangeLevel
 import uk.gov.justice.digital.hmpps.hmppstier.domain.enums.ProtectLevel
+import java.io.Serializable
 
 data class TierCalculationResultEntity(
   val protect: TierLevel<ProtectLevel>,
   val change: TierLevel<ChangeLevel>
-)
+) : Serializable
