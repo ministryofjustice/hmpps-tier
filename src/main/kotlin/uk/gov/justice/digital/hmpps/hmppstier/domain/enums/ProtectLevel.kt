7c1cbabee18f5045efaf957310d6233fede60bf9
Nick McMahon <nicholas.mcmahon@digital.justice.gov.uk>
2021-02-16 11:40:16 +0000

    ktlint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/domain/enums/ProtectLevel.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/domain/enums/ProtectLevel.kt
@@ -1,6 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppstier.domain.enums
 
-enum class ProtectLevel(val value : String) {
+enum class ProtectLevel(val value: String) {
   A("A"),
   B("B"),
   C("C"),
