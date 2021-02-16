cfc610fcac782c54b268d61562d5c7f7dde6e08c
Nick McMahon <nicholas.mcmahon@digital.justice.gov.uk>
2021-02-16 11:38:47 +0000

    Don't use ordinal because if it changes we'll silently be sending the wrong value until someone realises

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppstier/domain/enums/ChangeLevelTest.kt
@@ -0,0 +1,34 @@
+package uk.gov.justice.digital.hmpps.hmppstier.domain.enums
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class ChangeLevelTest {
+
+  @Nested
+  @DisplayName("Values test")
+  inner class ValuesTest {
+
+    @Test
+    fun `It should match 0`() {
+      assertThat(ChangeLevel.ZERO.value).isEqualTo(0)
+    }
+
+    @Test
+    fun `It should match 1`() {
+      assertThat(ChangeLevel.ONE.value).isEqualTo(1)
+    }
+
+    @Test
+    fun `It should match 2`() {
+      assertThat(ChangeLevel.TWO.value).isEqualTo(2)
+    }
+
+    @Test
+    fun `It should match 3`() {
+      assertThat(ChangeLevel.THREE.value).isEqualTo(3)
+    }
+  }
+}
