8ebb50cbd1e899072036e7e71de95368f621a064
Mark Rees <mark.rees@digital.justice.gov.uk>
2021-03-23 14:09:00 +0000

    MAS-364 reduce logging when retrieving tier calculations

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/service/TierCalculationService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/service/TierCalculationService.kt
@@ -81,27 +81,20 @@ class TierCalculationService(
     }
   }
 
-  private fun getLatestTierCalculation(crn: String): TierCalculationEntity? {
-    log.debug("Finding latest tier calculation for $crn")
-
-    return tierCalculationRepository.findFirstByCrnOrderByCreatedDesc(crn).also {
+  private fun getLatestTierCalculation(crn: String): TierCalculationEntity? =
+    tierCalculationRepository.findFirstByCrnOrderByCreatedDesc(crn).also {
       when (it) {
         null -> log.info("No tier calculation found for $crn")
         else -> log.info("Found latest tier calculation for $crn")
       }
     }
-  }
 
-  private fun getTierCalculationById(crn: String, calculationId: UUID): TierCalculationEntity? {
-    log.debug("Finding latest tier calculation for $crn")
-
-    return tierCalculationRepository.findByCrnAndUuid(crn, calculationId).also {
+  private fun getTierCalculationById(crn: String, calculationId: UUID): TierCalculationEntity? =
+    tierCalculationRepository.findByCrnAndUuid(crn, calculationId).also {
       when (it) {
-        null -> log.info("No tier calculation found for $crn")
-        else -> log.info("Found latest tier calculation for $crn")
+        null -> log.info("No tier calculation found for $crn $calculationId")
       }
     }
-  }
 
   companion object {
     private val log = LoggerFactory.getLogger(TierCalculationService::class.java)
