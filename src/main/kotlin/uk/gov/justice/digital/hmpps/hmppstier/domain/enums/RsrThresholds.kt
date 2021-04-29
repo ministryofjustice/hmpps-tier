6efb51b1f8211e8ee86120f7d17770b62ecf390b
Nick McMahon <nicholas.mcmahon@digital.justice.gov.uk>
2021-04-29 14:09:30 +0100

    Stop RSR values of 999.99 scoring 20 points

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/domain/enums/RsrThresholds.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/domain/enums/RsrThresholds.kt
@@ -5,4 +5,5 @@ import java.math.BigDecimal
 enum class RsrThresholds(val num: BigDecimal) {
   TIER_B_RSR(BigDecimal(7.0)),
   TIER_C_RSR(BigDecimal(3.0)),
+  NO_RSR_MAGIC_NUMBER(BigDecimal(999.99))
 }
