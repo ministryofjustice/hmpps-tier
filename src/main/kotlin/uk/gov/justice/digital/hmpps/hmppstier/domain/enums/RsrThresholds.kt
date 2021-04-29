36653877612f95ffb0fa9f472c82268f8f23d4ca
Nick McMahon <nicholas.mcmahon@digital.justice.gov.uk>
2021-04-29 14:54:19 +0100

    Properly bound the RSR check.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/domain/enums/RsrThresholds.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/domain/enums/RsrThresholds.kt
@@ -3,7 +3,8 @@ package uk.gov.justice.digital.hmpps.hmppstier.domain.enums
 import java.math.BigDecimal
 
 enum class RsrThresholds(val num: BigDecimal) {
-  TIER_B_RSR(BigDecimal(7.0)),
-  TIER_C_RSR(BigDecimal(3.0)),
-  NO_RSR_MAGIC_NUMBER(BigDecimal(999.99))
+  TIER_B_RSR_UPPER(BigDecimal(99.99)),
+  TIER_B_RSR_LOWER(BigDecimal(7.00)),
+  TIER_C_RSR_UPPER(BigDecimal(6.99)),
+  TIER_C_RSR_LOWER(BigDecimal(3.00)),
 }
