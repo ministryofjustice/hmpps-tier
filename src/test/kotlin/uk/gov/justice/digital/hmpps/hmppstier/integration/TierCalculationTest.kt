9877d495def3084aa4b1e4b87b88edf442219269
Mark Rees <mark.rees@digital.justice.gov.uk>
2021-05-27 12:27:03 +0100

    TEST: only write back if the protect or change level is different

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppstier/integration/TierCalculationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppstier/integration/TierCalculationTest.kt
@@ -3,7 +3,9 @@ package uk.gov.justice.digital.hmpps.hmppstier.integration
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import uk.gov.justice.digital.hmpps.hmppstier.integration.setup.MockedEndpointsTestBase
+import uk.gov.justice.digital.hmpps.hmppstier.integration.setup.assessmentsApiHighSeverityNeedsResponse
 import uk.gov.justice.digital.hmpps.hmppstier.integration.setup.emptyRegistrationsResponse
+import uk.gov.justice.digital.hmpps.hmppstier.integration.setup.registrationsResponseWithMappa
 
 class TierCalculationTest : MockedEndpointsTestBase() {
 
@@ -61,8 +63,31 @@ class TierCalculationTest : MockedEndpointsTestBase() {
       calculateTierFor(crn)
       expectNoUpdatedTierCalculation()
     }
+
     @Test
-    fun `writes back when tier is changed`() {
+    fun `Does not write back when calculation result differs but tier is unchanged`() {
+      val crn = "X432779"
+
+      setupSCCustodialSentence(crn)
+      setupMaleOffenderWithRegistrations(crn, false, "4234568890")
+      setupLatestAssessment(crn, 2018, "1234567890")
+
+      calculateTierFor(crn)
+      expectTierCalculation("A2")
+
+      setupSCCustodialSentence(crn)
+      setupRegistrations(registrationsResponseWithMappa(), crn)
+      setupCommunityApiAssessment(crn, ogrs = "0")
+      setupMaleOffender(crn)
+      setupNeeds(assessmentsApiHighSeverityNeedsResponse(), "4234568899")
+      setupCurrentAssessment(crn, "4234568899") // assessment not out of date
+
+      calculateTierFor(crn)
+      expectNoUpdatedTierCalculation()
+    }
+
+    @Test
+    fun `writes back when change level is changed`() {
       val crn = "X432770"
 
       setupSCCustodialSentence(crn)
@@ -78,6 +103,26 @@ class TierCalculationTest : MockedEndpointsTestBase() {
       setupCurrentAssessment(crn, "4234568891") // assessment not out of date
       calculateTierFor(crn)
       expectTierCalculation("A1")
+    }
+
+    @Test
+    fun `writes back when protect level is changed`() {
+      val crn = "X432771"
+
+      setupSCCustodialSentence(crn)
+      setupMaleOffenderWithRegistrations(crn, false, "4234568890")
+      setupLatestAssessment(crn, 2018, "4234568890")
+
+      calculateTierFor(crn)
+      expectTierCalculation("A2")
+
+      setupSCCustodialSentence(crn)
+      setupRegistrations(registrationsResponseWithMappa("M1"), crn)
+      restOfSetupWithMaleOffenderNoSevereNeeds(crn, false, "4234568890")
+      setupLatestAssessment(crn, 2018, "4234568890")
+
+      calculateTierFor(crn)
+      expectTierCalculation("B2")
     }
   }
 }
