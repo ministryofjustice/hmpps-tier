f321c0a1e84f5cffb391ccd28d1968d2ab969e78
carlov20 <carlov20@users.noreply.github.com>
2022-08-04 12:04:50 +0100

    MAS-450 changed queue admin role

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/config/ResourceServerConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/config/ResourceServerConfiguration.kt
@@ -37,7 +37,7 @@ class ResourceServerConfiguration {
             "/v3/api-docs/**",
             "/swagger-ui/**",
             "/swagger-ui.html",
-            "/queue-admin/**",
+            "/queue-admin/retry-all-dlqs",
             "/crn/upload"
           )
           .permitAll()
