1e2c633227dce4d45e29068e4bb51f28ed0059eb
Mark Rees <mark.rees@digital.justice.gov.uk>
2022-09-12 07:11:24 +0100

    FIXBUILD: manually upgrade to beta version of spring boot plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.4.3"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.0-beta"
   kotlin("plugin.spring") version "1.7.10"
   kotlin("plugin.jpa") version "1.7.10"
   id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
