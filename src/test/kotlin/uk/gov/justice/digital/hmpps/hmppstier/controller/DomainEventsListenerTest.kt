ca52569696e6a6be5144a0ae4056c5e44f434db1
AndySweilam <AndySweilam@users.noreply.github.com>
2022-12-07 16:39:24 +0000

    WFP-1667: add domain events listener for tier calculation.

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppstier/controller/DomainEventsListenerTest.kt
@@ -0,0 +1,17 @@
+package uk.gov.justice.digital.hmpps.hmppstier.controller
+
+import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.hmppstier.integration.setup.IntegrationTestBase
+
+class DomainEventsListenerTest: IntegrationTestBase() {
+  @Test
+  fun `can calculate tier on domain event`() {
+    val crn = "X432777"
+
+    setupSCCustodialSentence(crn)
+    setupMaleOffenderWithRegistrations(crn, assessmentId = "4234568890")
+
+    calculateTierForDomainEvent(crn)
+    expectLatestTierCalculation("A1")
+  }
+}
