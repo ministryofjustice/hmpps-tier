b8707f05156c3c79ad463aaf41a57fd2c9dabb1c
carlov20 <carlov20@users.noreply.github.com>
2023-02-28 09:49:28 +0000

    made exception handler coroutines

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/config/HmppsTierExceptionHandler.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/config/HmppsTierExceptionHandler.kt
@@ -9,7 +9,6 @@ import org.springframework.http.converter.HttpMessageConversionException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
-import reactor.core.publisher.Mono
 import uk.gov.justice.digital.hmpps.hmppstier.dto.ErrorResponse
 import uk.gov.justice.digital.hmpps.hmppstier.service.exception.EntityNotFoundException
 
@@ -21,58 +20,48 @@ class HmppsTierExceptionHandler {
   }
 
   @ExceptionHandler(EntityNotFoundException::class)
-  fun handleEntityNotFoundException(e: EntityNotFoundException): Mono<ResponseEntity<ErrorResponse>> {
+  suspend fun handleEntityNotFoundException(e: EntityNotFoundException): ResponseEntity<ErrorResponse> {
     log.error("EntityNotFoundException: {}", e.message)
-    return Mono.just(
-      ResponseEntity
-        .status(NOT_FOUND)
-        .body(ErrorResponse(status = 404, developerMessage = e.message))
-    )
+    return ResponseEntity
+      .status(NOT_FOUND)
+      .body(ErrorResponse(status = 404, developerMessage = e.message))
   }
 
   @ExceptionHandler(HttpMessageConversionException::class)
-  fun handleHttpMessageConversionException(e: HttpMessageConversionException): Mono<ResponseEntity<ErrorResponse>> {
+  suspend fun handleHttpMessageConversionException(e: HttpMessageConversionException): ResponseEntity<ErrorResponse> {
     log.error("HttpMessageConversionException: {}", e.message)
-    return Mono.just(
-      ResponseEntity
-        .status(BAD_REQUEST)
-        .body(ErrorResponse(status = 400, developerMessage = e.message))
-    )
+    return ResponseEntity
+      .status(BAD_REQUEST)
+      .body(ErrorResponse(status = 400, developerMessage = e.message))
   }
 
   @ExceptionHandler(IllegalArgumentException::class)
-  fun handleIllegalArgumentException(e: IllegalArgumentException): Mono<ResponseEntity<ErrorResponse>> {
+  suspend fun handleIllegalArgumentException(e: IllegalArgumentException): ResponseEntity<ErrorResponse> {
     log.error("IllegalArgumentException: {}", e.message)
-    return Mono.just(
-      ResponseEntity
-        .status(BAD_REQUEST)
-        .body(ErrorResponse(status = 400, developerMessage = e.message))
-    )
+    return ResponseEntity
+      .status(BAD_REQUEST)
+      .body(ErrorResponse(status = 400, developerMessage = e.message))
   }
 
   @ExceptionHandler(MethodArgumentTypeMismatchException::class)
-  fun handleMethodArgumentTypeMismatchException(e: MethodArgumentTypeMismatchException): Mono<ResponseEntity<ErrorResponse>> {
+  suspend fun handleMethodArgumentTypeMismatchException(e: MethodArgumentTypeMismatchException): ResponseEntity<ErrorResponse> {
     log.error("MethodArgumentTypeMismatchException: {}", e.message)
-    return Mono.just(
-      ResponseEntity
-        .status(BAD_REQUEST)
-        .body(ErrorResponse(status = 400, developerMessage = e.message))
-    )
+    return ResponseEntity
+      .status(BAD_REQUEST)
+      .body(ErrorResponse(status = 400, developerMessage = e.message))
   }
 
   @ExceptionHandler(Exception::class)
-  fun handleException(e: Exception): Mono<ResponseEntity<ErrorResponse>> {
+  suspend fun handleException(e: Exception): ResponseEntity<ErrorResponse> {
     log.error("Exception", e)
-    return Mono.just(
-      ResponseEntity
-        .status(BAD_REQUEST)
-        .body(
-          ErrorResponse(
-            status = 500,
-            developerMessage = "Internal Server Error. Check Logs",
-            userMessage = "An unexpected error has occurred"
-          )
+    return ResponseEntity
+      .status(BAD_REQUEST)
+      .body(
+        ErrorResponse(
+          status = 500,
+          developerMessage = "Internal Server Error. Check Logs",
+          userMessage = "An unexpected error has occurred"
         )
-    )
+      )
   }
 }
