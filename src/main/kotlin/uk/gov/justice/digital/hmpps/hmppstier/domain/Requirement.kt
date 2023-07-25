33cd75273872f0600514f78b18d47e7aafdc3891
Andy Sweilam <104897795+AndySweilam@users.noreply.github.com>
2023-07-25 17:15:14 +0100

    Wfp 2073 (#476)
    * WFP-2073: remove community api remnants
    
    Signed-off-by: AndySweilam <AndySweilam@users.noreply.github.com>
    
    * WFP-2073: re-raise jacoco coverage and complexity.
    
    Signed-off-by: AndySweilam <AndySweilam@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: AndySweilam <AndySweilam@users.noreply.github.com>
    Co-authored-by: AndySweilam <AndySweilam@users.noreply.github.com>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/domain/Requirement.kt
+++
@@ -1,9 +0,0 @@
-package uk.gov.justice.digital.hmpps.hmppstier.domain
-
-import uk.gov.justice.digital.hmpps.hmppstier.config.Generated
-
-@Generated
-data class Requirement constructor(
-  val isRestrictive: Boolean,
-  val mainCategory: String,
-)
