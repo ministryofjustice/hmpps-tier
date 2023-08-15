7752bcd5d290e63b8090c62a739e4606af4b51b8
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2023-08-15 14:14:46 +0100

    updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,6 @@ dependencies {
 
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.7.3")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.7.3")
 
   testImplementation("org.springframework.boot:spring-boot-starter-test") {
     exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
