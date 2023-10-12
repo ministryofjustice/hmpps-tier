04e16e705ecac78765c7b899f4408d00738bc36d
Marcus Aspin <maspin@unilink.com>
2023-10-12 22:49:54 +0000

    PI-1544 Downgrade Kotlin to 1.9.0 due to lack of detekt support
    See https://github.com/detekt/detekt/issues/6428

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@
 
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.5.1"
-  kotlin("plugin.spring") version "1.9.10"
-  kotlin("plugin.jpa") version "1.9.10"
+  kotlin("plugin.spring") version "1.9.0"
+  kotlin("plugin.jpa") version "1.9.0"
   id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
   jacoco
   java
