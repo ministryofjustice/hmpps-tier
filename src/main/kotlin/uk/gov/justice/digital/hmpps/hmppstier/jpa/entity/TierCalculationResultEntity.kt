800e2d503f4c474e51cebffc61f4d6d8b7c2a4f0
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2024-01-19 15:31:47 +0000

    Try ignoring unknown properties with annotation (#538)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/jpa/entity/TierCalculationResultEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/jpa/entity/TierCalculationResultEntity.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppstier.jpa.entity
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import uk.gov.justice.digital.hmpps.hmppstier.client.AssessmentForTier
 import uk.gov.justice.digital.hmpps.hmppstier.domain.DeliusInputs
 import uk.gov.justice.digital.hmpps.hmppstier.domain.TierLevel
@@ -7,6 +8,7 @@ import uk.gov.justice.digital.hmpps.hmppstier.domain.enums.ChangeLevel
 import uk.gov.justice.digital.hmpps.hmppstier.domain.enums.ProtectLevel
 import java.io.Serializable
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class TierCalculationResultEntity(
     val protect: TierLevel<ProtectLevel>,
     val change: TierLevel<ChangeLevel>,
