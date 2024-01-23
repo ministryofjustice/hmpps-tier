e5cffd14048efd88afaf95ea1335c9a81115e786
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-01-23 07:46:33 +0000

    PI-1525 update retry interceptor

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/config/RetryInterceptor.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/config/RetryInterceptor.kt
@@ -5,6 +5,7 @@ import org.springframework.http.client.ClientHttpRequestExecution
 import org.springframework.http.client.ClientHttpRequestInterceptor
 import org.springframework.http.client.ClientHttpResponse
 import org.springframework.web.client.RestClientException
+import java.net.http.HttpTimeoutException
 import java.time.Duration
 import java.util.concurrent.TimeUnit
 import kotlin.reflect.KClass
@@ -15,7 +16,7 @@ class RetryInterceptor(private val retries: Int = 3, private val delay: Duration
         request: HttpRequest,
         body: ByteArray,
         execution: ClientHttpRequestExecution
-    ): ClientHttpResponse = retry(retries, listOf(RestClientException::class), delay) {
+    ): ClientHttpResponse = retry(retries, listOf(RestClientException::class, HttpTimeoutException::class), delay) {
         execution.execute(request, body)
     }
 }
