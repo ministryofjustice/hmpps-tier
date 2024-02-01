38a2324b7c8d5cb4a35213cbf669f6d27945f1e4
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2024-02-01 15:09:56 +0000

    PI-1854 handle transition until full recalc ran (#551)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/service/TierReader.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/service/TierReader.kt
@@ -1,11 +1,11 @@
 package uk.gov.justice.digital.hmpps.hmppstier.service
 
-import org.slf4j.LoggerFactory
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppstier.dto.TierDetailsDto
 import uk.gov.justice.digital.hmpps.hmppstier.dto.TierDto
 import uk.gov.justice.digital.hmpps.hmppstier.jpa.entity.TierCalculationEntity
+import uk.gov.justice.digital.hmpps.hmppstier.jpa.entity.TierSummary
 import uk.gov.justice.digital.hmpps.hmppstier.jpa.entity.TierSummaryRepository
 import uk.gov.justice.digital.hmpps.hmppstier.jpa.repository.TierCalculationRepository
 import java.util.*
@@ -20,6 +20,17 @@ class TierReader(
 
     fun getLatestTierByCrn(crn: String): TierDto? =
         tierSummaryRepository.findByIdOrNull(crn)?.let {
+            TierDto.from(it)
+        } ?: getLatestTierCalculation(crn)?.let {
+            try {
+                tierSummaryRepository.save(
+                    TierSummary(
+                        it.crn, it.uuid, it.protectLevel(), it.changeLevel(), 0, it.created
+                    )
+                )
+            } catch (ignored: Exception) {
+                // Doesn't matter if insert fails, should still return the result from the read
+            }
             TierDto.from(it)
         }
 
