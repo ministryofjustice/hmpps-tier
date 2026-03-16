3ae7ca5e42192fe2835197fee79c56deab9cda71
Marcus Aspin <maspin@unilink.com>
2026-03-16 17:05:22 +0000

    PI-3963 Fix blocking retries
    Also fix handling of domain events with null values in additionalInformation.
    
    Fixes [HMPPS-TIER-13](https://ministryofjustice.sentry.io/issues/7338944990) and [HMPPS-TIER-12](https://ministryofjustice.sentry.io/issues/7338823125)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/config/ReactorExtensions.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/config/ReactorExtensions.kt
@@ -1,10 +1,12 @@
 package uk.gov.justice.digital.hmpps.hmppstier.config
 
 import reactor.core.publisher.Mono
+import reactor.core.scheduler.Schedulers
 import reactor.util.retry.Retry
 
 object ReactorExtensions {
-    fun <T> Retry.invoke(function: () -> T): T = Mono.fromCallable { function() }
+    fun <T> Retry.invoke(function: () -> T): T = Mono.fromCallable(function)
+        .subscribeOn(Schedulers.boundedElastic())
         .retryWhen(this)
         .block()!!
 }
