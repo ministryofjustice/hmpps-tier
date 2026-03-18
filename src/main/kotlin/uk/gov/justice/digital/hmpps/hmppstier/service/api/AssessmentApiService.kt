111e40bb09340db30d9eaa80d8cc7151776cef88
Marcus Aspin <maspin@unilink.com>
2026-03-18 16:53:40 +0000

    PI-3969 Remove 55-week limit on assessments

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/service/api/AssessmentApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstier/service/api/AssessmentApiService.kt
@@ -3,7 +3,6 @@ package uk.gov.justice.digital.hmpps.hmppstier.service.api
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppstier.client.ArnsApiClient
 import uk.gov.justice.digital.hmpps.hmppstier.client.arns.AssessmentForTier
-import java.time.LocalDate
 
 @Service
 class AssessmentApiService(
@@ -11,6 +10,5 @@ class AssessmentApiService(
 ) {
     fun getTierAssessmentInformation(crn: String): AssessmentForTier? = arnsApiClient.getTierAssessmentInformation(crn)
     fun getRiskPredictors(crn: String) = arnsApiClient.getRiskPredictors(crn)
-        ?.filter { it.completedDate >= LocalDate.now().atStartOfDay().minusWeeks(55) }
         ?.maxByOrNull { it.completedDate }
 }
